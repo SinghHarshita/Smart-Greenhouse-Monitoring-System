ae95f3ff5bf6971e946fac07714b93a596856b94
Vignesh <2017.vignesh.pillai@ves.ac.in>
2019-12-16 05:24:00 +0530

    Login and Register

--- SmartGreenhouseMonitoringSystemBackend/SmartGreenhouseApi/views.py
+++ SmartGreenhouseMonitoringSystemBackend/SmartGreenhouseApi/views.py
@@ -1,3 +1,77 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
+from django.db import connection
 
-# Create your views here.
+@api_view(['POST'])
+def login(request):
+    print(request.POST.dict())
+    data = request.POST.dict()
+    res = requests.get("https://oauth2.googleapis.com/tokeninfo?id_token=" + data['token'])
+    print(res.status_code)
+    if res.status_code != 200:
+        msg = {
+            "msg": "Not Registerd User",
+            "status": 0
+        }
+    elif res.status_code == 200:
+        try:
+            email = data['email']
+        except:
+            msg = {
+                "msg": "Not Registerd User",
+                "status": 0
+            }   
+        sql = "SELECT * from users where email = '{}'".format(data['email'])
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(sql)
+                # Gives Single Tuple with all information
+                result = cursor.fetchall()[0]
+                print(result)
+                msg = {
+                    "msg": "Valid User",
+                    "status": 1
+                }
+        except:
+            msg = {
+            "msg": "Not Registerd User",
+            "status": 0
+        }
+    return Response(res.status_code)
+
+@api_view(['POST'])
+def register(request):
+    data = requesst.POST.dict()
+    try:
+        email = data['email']
+        name = data['name'].split()
+        gender = data['gender']
+        dob = data['dob']
+        phone = data['phone']
+        address = data['address']
+    except:
+        msg = {
+            "msg": "Details are not Filled Properly",
+            "status": 0
+        }
+        return Response(msg)
+    if(len(name) == 2):
+        fname = str(name[0])
+        lname = str(name[1])
+    else:
+        fname = name[0]
+        lname = None
+
+    sql = "INSERT into users (fname,lname,email,dob,gender,phone,address) values ('{}','{}','{}','{}','{}','{}','{}')"
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        
+    msg = {
+        "msg" : "Registered Successfully",
+        "status" : 1
+    }
+    return Response(msg)
--- SmartGreenhouseMonitoringSystemBackend/SmartGreenhouseMonitoringSystemBackend/urls.py
+++ SmartGreenhouseMonitoringSystemBackend/SmartGreenhouseMonitoringSystemBackend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+# from django.conf.auth import logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('SmartGreenhouseApi.urls'))
+    path('',include('SmartGreenhouseApi.urls'))
 ]
